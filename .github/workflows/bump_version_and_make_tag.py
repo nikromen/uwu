9f53bfd0f0ba999daaef0255632ed10787dc4a3e
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-05-09 22:47:13 +0200

    install deps

--- .github/workflows/bump_version_and_make_tag.py
+++ .github/workflows/bump_version_and_make_tag.py
@@ -2,20 +2,22 @@ import subprocess
 
 import toml
 
-pyproject = toml.load("../../pyproject.toml")
+pyproject = toml.load("pyproject.toml")
 version = pyproject["tool"]["poetry"]["version"]
 minor = int(version.split(".")[1])
 new_version = f"0.{minor + 1}.0"
 pyproject["tool"]["poetry"]["version"] = new_version
 
-with open("../../pyproject.toml", "w") as f:
+with open("pyproject.toml", "w") as f:
     toml.dump(pyproject, f)
 
 
+subprocess.run(["git", "config", "--global", "user.name", "github-actions"])
+subprocess.run(["git", "config", "--global", "user.email", "github-actions@github.com"])
 subprocess.run(["git", "add", "pyproject.toml"])
 subprocess.run(
-    ["git", "commit", "-m", f"release of package uwu to version {version}"]
+    ["git", "commit", "-m", f"release of package uwu to version {new_version}"]
 )
 subprocess.run(["git", "push", "origin", "main"])
-subprocess.run(["git", "tag", version])
+subprocess.run(["git", "tag", new_version])
 subprocess.run(["git", "push", "origin", "--tags"])
